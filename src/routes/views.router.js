4e15e950fb0813bd1007c61f29d34b5e406aab7a
Carlos Cogliandro <carloscogliandro22@gmail.com>
2023-04-26 15:31:08 +0930

    prueba

---
+++ src/routes/views.router.js
@@ -0,0 +1,27 @@
+
+import { Router } from "express";
+import { executePolicies } from "../middleware/auth.js";
+import viewsController from "../controllers/views.controller.js";
+
+const router = Router();
+
+// ------------------------------------------------
+
+router.get('/home', viewsController.productsGetAll);
+router.get('/productcreator', viewsController.createProduct);
+
+// ------------------------------------------------
+
+router.get('/', viewsController.login);
+
+router.get('/register', viewsController.register );
+
+router.get('/profile', executePolicies(["AUTHENTICATED"]), viewsController.profile);
+
+router.get('/home', viewsController.home);
+
+router.get('/logout', viewsController.logOut);
+
+router.get('/info', viewsController.info);
+
+export default router;
